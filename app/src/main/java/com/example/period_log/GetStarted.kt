2165ab7322491bc8381c5f9f35ae8cb21e65cc1c
jennymylee <jennylee038@gmail.com>
2022-03-03 14:59:36 -0800

    connected first 3 layouts to main activity

---
+++ app/src/main/java/com/example/period_log/GetStarted.kt
@@ -0,0 +1,12 @@
+package com.example.period_log
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GetStarted : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.get_started_layout)
+    }
+}
