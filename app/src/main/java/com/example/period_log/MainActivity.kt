803421285b568a1edf0779a6a16e35e131f8391a
Cindy Park <ccindyy82021@gmail.com>
2022-03-03 22:16:35 -0800

    changed file names for clarity1

--- app/src/main/java/com/example/period_log/MainActivity.kt
+++ app/src/main/java/com/example/period_log/MainActivity.kt
@@ -9,16 +9,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_layout)
-        //        Testing the connection
-//        val firstObject = ParseObject("FirstClass")
-//        firstObject.put("message","Hey ! First message from android. Parse is now connected")
-//        firstObject.saveInBackground {
-//            if (it != null){
-//                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
-//            }else{
-//                Log.d("MainActivity","Object saved.")
-//            }
-//        }
+        //         Testing the connection
+        val firstObject = ParseObject("FirstClass")
+        firstObject.put("message","Hey ! First message from android. Parse is now connected")
+        firstObject.saveInBackground {
+            if (it != null){
+                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+            }else{
+                Log.d("MainActivity","Object saved.")
+            }
+        }
         val getStartedButton = findViewById<Button>(R.id.getStartedBtn)
         getStartedButton.setOnClickListener{
             val intent = Intent(this, SignIn::class.java)
