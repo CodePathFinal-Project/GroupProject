7cdcdf90836ba54839354b647c99a32e9d5b87e0
Ramy Meng <ramym@uci.edu>
2022-03-04 15:41:00 -0800

    Adding parse object into main activity

--- app/src/main/java/com/example/period_log/MainActivity.kt
+++ app/src/main/java/com/example/period_log/MainActivity.kt
@@ -3,22 +3,24 @@ package com.example.period_log
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.parse.ParseObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_layout)
         //         Testing the connection
-//        val firstObject = ParseObject("FirstClass")
-//        firstObject.put("message","Hey ! First message from android. Parse is now connected")
-//        firstObject.saveInBackground {
-//            if (it != null){
-//                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
-//            }else{
-//                Log.d("MainActivity","Object saved.")
-//            }
-//        }
+        val firstObject = ParseObject("FirstClass")
+        firstObject.put("message","Hey ! First message from android. Parse is now connected")
+        firstObject.saveInBackground {
+            if (it != null){
+                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+            }else{
+                Log.d("MainActivity","Object saved.")
+            }
+        }
         val getStartedButton = findViewById<Button>(R.id.getStartedBtn)
         getStartedButton.setOnClickListener{
             val intent = Intent(this, SignIn::class.java)
