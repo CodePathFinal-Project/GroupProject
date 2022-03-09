1b5580b4a7c1b22bdb16b3aecbfc0290205ede55
Ramy Meng <ramym@uci.edu>
2022-03-09 10:50:23 -0800

    Changed period length and cycle length buttons to editText

--- app/src/main/java/com/example/period_log/QuestionnaireActivity.kt
+++ app/src/main/java/com/example/period_log/QuestionnaireActivity.kt
@@ -1,11 +1,18 @@
 package com.example.period_log
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class QuestionnaireActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.questionnaire_layout)
+
+        val saveBtn = findViewById<Button>(R.id.saveBtn)
+        val periodLengthEt = findViewById<EditText>(R.id.periodLengthEt)
+        val cycleLengthEt = findViewById<EditText>(R.id.cycleLengthEt)
+
     }
 }
