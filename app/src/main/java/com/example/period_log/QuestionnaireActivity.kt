bce9ed7079948114d11a9946e58b5cef46e1a0fa
clemieclu <nguyenyc@uci.edu>
2022-03-09 13:53:21 -0800

    1. added Settings and Questionnaire activity to Android Manifest 2. changed user input for questionnaire_layout.xml to be numbers

--- app/src/main/java/com/example/period_log/QuestionnaireActivity.kt
+++ app/src/main/java/com/example/period_log/QuestionnaireActivity.kt
@@ -1,8 +1,10 @@
 package com.example.period_log
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class QuestionnaireActivity : AppCompatActivity() {
@@ -13,6 +15,9 @@ class QuestionnaireActivity : AppCompatActivity() {
         val saveBtn = findViewById<Button>(R.id.saveBtn)
         val periodLengthEt = findViewById<EditText>(R.id.periodLengthEt)
         val cycleLengthEt = findViewById<EditText>(R.id.cycleLengthEt)
+
+        //Toast.makeText(this, periodLengthEt.text, Toast.LENGTH_SHORT).show()
+
 
     }
 }
