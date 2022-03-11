d802fe2e80568515f06a5342dbba522641137666
ramyyyy <88201031+ramy-meng@users.noreply.github.com>
2022-03-10 16:15:19 -0800

    Revert "Change userPeriodAndCycleLength to ParseObject and add savePeriodAndCycleLength"

--- app/src/main/java/com/example/period_log/QuestionnaireActivity.kt
+++ app/src/main/java/com/example/period_log/QuestionnaireActivity.kt
@@ -2,7 +2,6 @@ package com.example.period_log
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.UserHandle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -13,10 +12,6 @@ import com.parse.GetCallback
 
 
 class QuestionnaireActivity : AppCompatActivity() {
-
-    lateinit var currUserPeriodAndCycleLength : ParseObject
-    var allUsers: MutableList<UserPeriodAndCycleLength> = mutableListOf()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.questionnaire_layout)
@@ -27,60 +22,36 @@ class QuestionnaireActivity : AppCompatActivity() {
 
         saveBtn.setOnClickListener {
             val user = ParseUser.getCurrentUser()
-            Toast.makeText(this,"Curr userId: ${user.objectId}", Toast.LENGTH_SHORT).show()
+            val userId = user.objectId.toString()
+
             //Get periodLength and cycleLength from the EditText
             val periodLength = periodLengthEt.text.toString().toInt()
             val cycleLength = cycleLengthEt.text.toString().toInt()
 
-            savePeriodAndCycleLength(user, periodLength, cycleLength)
+            Log.i(TAG, "Save Button has been clicked $userId")
+
+            //savePeriodAndCycleLength(userId, periodLength, cycleLength)
         }
     }
 
-    private  fun savePeriodAndCycleLength(user: ParseUser, periodLength : Int?, cycleLength : Int?) {
-        //Get all objects in our back4app UserPeriodAndCycleLength
-        val query: ParseQuery<UserPeriodAndCycleLength> = ParseQuery.getQuery<UserPeriodAndCycleLength>(UserPeriodAndCycleLength::class.java)
-        //This line is added because user is a pointer
-        query.include(UserPeriodAndCycleLength.KEY_USER)
-//        //Only return the periodAndCycleLength from the current user
-        query.whereEqualTo(UserPeriodAndCycleLength.KEY_USER, user)
-        //Retrieve the objectId by userId
-        query.findInBackground(object: FindCallback<UserPeriodAndCycleLength> {
-            override fun done(usersPeriodAndCycleLength : MutableList<UserPeriodAndCycleLength>?, e: ParseException?) {
-                if (e != null) {
-                    //Something went wrong
-                    Log.e(TAG, "Error fetching userPeriodCycleLength")
-                } else {
-                    if (usersPeriodAndCycleLength != null) {
-                        //We know that it will return a list with only one user in it
-                        if (usersPeriodAndCycleLength.size == 1)
-                        {
-                            currUserPeriodAndCycleLength = usersPeriodAndCycleLength[0]
-                            allUsers.addAll(usersPeriodAndCycleLength)
-                            Log.i(TAG, "The fetch has only one user")
-                        }
-
-                    }
-                }
-
-
-            }
-        })
-//        var userPeriodAndCycle: List<UserPeriodAndCycleLength> = query.find()
-//        val objectId = userPeriodAndCycle[0].objectId
-//        Log.i(TAG, "Current user objectId $objectId")
-//        query.getInBackground(objectId
-//        ) { userPeriodAndCycleLength, e ->
-//            if (e == null) {
-//                // Update it new data
-//                userPeriodAndCycleLength.put(UserPeriodAndCycleLength.KEY_PERIOD_LENGTH, periodLength)
-//                userPeriodAndCycleLength.put(UserPeriodAndCycleLength.KEY_CYCLE_LENGTH, cycleLength)
-//                userPeriodAndCycleLength.saveInBackground()
-//                Log.i(TAG, "Period and cycle length has been updated!")
-//            } else {
-//                Log.i(TAG, "The period length and cycle length did not get updated!")
+//    private  fun savePeriodAndCycleLength(userId: String, periodLength : Int, cycleLength : Int) {
+//        //Get all objects in our back4app UserPeriodAndCycleLength
+//        val query: ParseQuery<UserPeriodAndCycleLength> = ParseQuery.getQuery<UserPeriodAndCycleLength>(UserPeriodAndCycleLength::class.java)
+//        //Retrieve the object by id
+//        query.getInBackground(userId, object : GetCallback<UserPeriodAndCycleLength> {
+//            override fun done(userPeriodAndCycleLength : UserPeriodAndCycleLength, e: ParseException?) {
+//                if (e == null) {
+//                    // Update it new data
+//                    userPeriodAndCycleLength.put(UserPeriodAndCycleLength.KEY_PERIOD_LENGTH, periodLength)
+//                    userPeriodAndCycleLength.put(UserPeriodAndCycleLength.KEY_CYCLE_LENGTH, cycleLength)
+//                    userPeriodAndCycleLength.saveInBackground()
+//                    Log.i(TAG, "Period and cycle length has been updated!")
+//                } else {
+//                    Log.i(TAG, "The period length and cycle length did not get updated!")
+//                }
 //            }
-//        }
-    }
+//        })
+//    }
 
     companion object {
         const val TAG = "QuestionnaireActivity"
