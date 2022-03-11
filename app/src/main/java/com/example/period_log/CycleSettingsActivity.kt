e669d649497a4765c99116e67f2a2ca97f48f07d
clemieclu <nguyenyc@uci.edu>
2022-03-10 21:06:29 -0800

    1. updated cycle_layout.xml and CycleSettingsActivity.kt 2. added CycleSettingsActivity.kt to AndroidManifest.xml

--- app/src/main/java/com/example/period_log/CycleSettingsActivity.kt
+++ app/src/main/java/com/example/period_log/CycleSettingsActivity.kt
@@ -1,11 +1,32 @@
 package com.example.period_log
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class CycleSettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cycle_layout)
+
+        val saveCycle = findViewById<Button>(R.id.saveCycle)
+        val PeriodLengthET_setting = findViewById<EditText>(R.id.PeriodLengthET_setting)
+        val CycleLengthET_setting = findViewById<EditText>(R.id.CycleLengthET_setting)
+
+        PeriodLengthET_setting.setOnClickListener(){
+            Log.i(TAG, PeriodLengthET_setting.text.toString()) // logcat message for debugging
+            val periodLengthEt_value = PeriodLengthET_setting.text.toString().toInt() // stores user input as integer
+        }
+
+        CycleLengthET_setting.setOnClickListener(){
+            Log.i(TAG, CycleLengthET_setting.text.toString())
+            val CycleLengthET_setting_value = CycleLengthET_setting.text.toString().toInt()
+        }
+    }
+
+    companion object {
+        const val TAG ="CycleSettingsActivity"
     }
 }
