3aecb81d3901a0c237be34e165d64af30195f321
Cindy Park <ccindyy82021@gmail.com>
2022-03-11 17:24:02 -0800

    0311 Add events

--- app/src/main/java/com/example/period_log/Calendar.kt
+++ app/src/main/java/com/example/period_log/Calendar.kt
@@ -26,28 +26,25 @@ import java.util.Calendar
 class Calendar : AppCompatActivity() {
 
     lateinit var mmmmYYYY : TextView
-    lateinit var mYEdited : String
     lateinit var btnSettings: ImageButton
     lateinit var btnRefresh : Button
     val wrapper: Context = ContextThemeWrapper(this, R.style.PopupMenuStyle)
     lateinit var compactCalendarView: CompactCalendarView
-
-    //var allCycles: MutableList<Cycle> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
         compactCalendarView = findViewById(R.id.compactcalendar_view) as CompactCalendarView
 
+        fetchCycles()
         mmmmYYYY = findViewById(R.id.tvMonth)
         mmmmYYYY.setText(MONTHS[Calendar.getInstance().get(Calendar.MONTH)] + ' '+ Calendar.getInstance().get(Calendar.YEAR).toString())
 
-        //REFRESH BUTTON
+//        //REFRESH BUTTON
         //TODO: refresh button where its on clickListener call fetchCycles
         btnRefresh = findViewById<Button>(R.id.Refreshbutton)
 
         btnRefresh.setOnClickListener {
-            fetchCycles()
             startActivity(intent)
             finish()
         }
@@ -56,66 +53,8 @@ class Calendar : AppCompatActivity() {
         btnSettings = findViewById<ImageButton>(R.id.cvSettings)
         btnSettings.setOnClickListener{
             val intent = Intent(this, SettingsActivity::class.java)
-
-            //TODO: Test the fetchQuery
-            //fetchCycles()
             startActivity(intent)
         }
-
-//        val compactCalendarView: CompactCalendarView = findViewById(R.id.compactcalendar_view) as CompactCalendarView
-        // Set first day of week to Monday, defaults to Monday so calling setFirstDayOfWeek is not necessary
-        // Use constants provided by Java Calendar class
-
-        // Add event on Feb 22, 2022
-        var ev = Event(Color.GREEN, 1645516800000, "Some extra data that I want to store.")
-        compactCalendarView.addEvent(ev)
-
-        // Added event Feb 23, 2022
-        ev = Event(Color.GREEN, 1645603200000)
-        compactCalendarView.addEvent(ev)
-
-        // Added event Feb 23 ,2022
-        val temp = 1644739200000
-        ev = Event(Color.CYAN, temp)
-        compactCalendarView.addEvent(ev)
-
-        //Added event to the last startedAt March 05, 2022
-        if (allCycles.size !=  0) {
-            Log.i(TAG, "allCycles is not empty!!")
-//            val cycle = allCycles[0]
-//            Log.i(TAG, )
-//            for (cycle in allCycles) {
-//                val temp = cycle.getStartedAt()
-//                Log.i(TAG, "Inside the for loop")
-//                val ev = Event(Color.CYAN, temp)
-//                compactCalendarView.addEvent(ev)
-        } else {
-            Log.i(TAG, "allCycles is empty!!")
-        }
-
-        //TODO : Try to show the cycle on the calendar on March 05 - March 12 and Feb 20 - Feb 25
-//        if (allCycles != null) {
-//            for (cycle in allCycles) {
-//                var startedAt = cycle.getStartedAt()
-//                var endedAt = cycle.getEndedAt()
-//                val oneDayinMilliSec = 86400000.toLong()
-//                for (day in startedAt..(endedAt+1) step oneDayinMilliSec) {
-//                    Log.i(TAG, "Reach this for loop")
-//                    ev = Event(Color.CYAN, 1644739200000)
-//                    compactCalendarView.addEvent(ev)
-//                }
-//            }
-//        }
-
-        // Query for events on Sun, 07 Jun 2015 GMT.
-        // Time is not relevant when querying for events, since events are returned by day.
-        // So you can pass in any arbitrary DateTime and you will receive all events for that day.
-        val events: List<Event> =
-            compactCalendarView.getEvents(1433701251000L) // can also take a Date object
-
-        // events has size 2 with the 2 events inserted previously
-        Log.d(TAG, "Events: $events")
-
 
         // define a listener to receive callbacks when certain events happen.
         compactCalendarView.setListener(object : CompactCalendarView.CompactCalendarViewListener {
@@ -171,15 +110,26 @@ class Calendar : AppCompatActivity() {
                             }
                             allCycles.clear()
                             allCycles.addAll(cycles)
-                            for (cycle in allCycles) {
-                                Log.i(
-                                    TAG, "Cycle startedAt: " + cycle.getStartedAt().toString()
-                                            + ", endedAt: " + cycle.getEndedAt().toString()
-                                            + ", username:" + cycle.getUser()?.username
-                                )
-                            }
 
                             //TODO: notifyChanged to rerender the calendarView
+                            //TODO: decide on end date measure? last day of period or the day after?
+                            if (allCycles.size !=  0) {
+                                Log.i(TAG, "allCycles is not empty!!")
+                                for (cycle in allCycles) {
+                                    var startedAt = cycle.getStartedAt()
+                                    var endedAt = cycle.getEndedAt()
+                                    val oneDayinMilliSec = 86400000.toLong()
+                                    //TODO: delete events element before populating?
+                                    for (day in startedAt..(endedAt+1) step oneDayinMilliSec) {
+                                        Log.i(TAG, "Reach this for loop")
+                                        val ev = Event(Color.CYAN, day)
+                                        compactCalendarView.addEvent(ev)
+                                    }
+                                }
+                            } else {
+                                Log.i(TAG, "allCycles is empty!!")
+                            }
+
                         }
                     }
 
@@ -216,5 +166,6 @@ class Calendar : AppCompatActivity() {
         val MONTHS = arrayOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
             "Aug", "Sep", "Oct", "Nov", "Dec")
         var allCycles: MutableList<Cycle> = mutableListOf()
+        var mYEdited = ""
     }
 }
